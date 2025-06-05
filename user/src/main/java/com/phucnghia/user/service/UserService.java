d3471abb4af038e2a782549dbed8bd93d20f7c26
dinh trong khang <trongkhangdinh.2003@gmail.com>
2025-06-06 01:47:58 +0700

    Update Fein

--- user/src/main/java/com/phucnghia/user/service/UserService.java
+++ user/src/main/java/com/phucnghia/user/service/UserService.java
@@ -2,6 +2,10 @@ package com.phucnghia.user.service;
 
 import com.phucnghia.user.dto.request.UserRequest;
 import com.phucnghia.user.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 
@@ -12,4 +16,10 @@ public interface UserService {
     void deleteUser(Long id);
     User updateUser(Long id, UserRequest request);
 
+}
+
+@FeignClient(name = "authservice")
+interface AuthServiceClient {
+    @PostMapping("/api/auth/validate")
+    ResponseEntity<Boolean> validateToken(@RequestHeader("Authorization") String token);
 }
