6e849e43d9ad165b1935f175f27c2459b4786951
tranp220708 <tranp220708@example.com>
2025-06-09 09:13:23 +0700

    update

---
+++ product_service/src/main/java/org/example/product_service/Repository/ProductVariantRepository.java
@@ -0,0 +1,7 @@
+package org.example.product_service.Repository;
+
+import org.example.product_service.entity.ProductVariant;
+import org.example.product_service.generic.IRepository;
+
+public interface ProductVariantRepository extends IRepository<ProductVariant,Integer> {
+}
