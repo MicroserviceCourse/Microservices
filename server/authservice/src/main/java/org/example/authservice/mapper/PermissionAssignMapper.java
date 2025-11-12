9ccdbf74faf466afe42d1cad66ac46f459981a81
tranp6648 <tranp6648@gmail.com>
2025-11-12 23:17:24 +0700

    feat(SuperAdmin): permission Module Assign Role

--- server/authservice/src/main/java/org/example/authservice/mapper/PermissionAssignMapper.java
+++ server/authservice/src/main/java/org/example/authservice/mapper/PermissionAssignMapper.java
@@ -37,7 +37,7 @@ public interface PermissionAssignMapper extends CommonMapper {
                 .collect(Collectors.toSet());
         Map<String, List<PermissionItemDto>> grouped = allPermissions.stream()
                 .collect(Collectors.groupingBy(
-                        p -> p.getModule().getName(),      
+                        p -> p.getModule().getName(),
                         Collectors.mapping(
                                 p -> toPermissionItemDto(p, assignedIds),
                                 Collectors.toList()
